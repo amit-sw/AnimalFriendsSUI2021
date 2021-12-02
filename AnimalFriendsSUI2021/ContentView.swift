d9b7ee0eaa223b488e868df43008ad8a936908a4
Amit Gupta <amit@gprof.com>
2021-12-02 12:20:15 -0800

    Initial commit

--- AnimalFriendsSUI2021/ContentView.swift
+++ AnimalFriendsSUI2021/ContentView.swift
@@ -5,17 +5,109 @@
 //  Created by Amit Gupta on 12/2/21.
 //
 
+
 import SwiftUI
+import Alamofire
+import SwiftyJSON
 
 struct ContentView: View {
+    @State var animalName = " "
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage? = UIImage(named: "paws")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            VStack (alignment: .center,
+                    spacing: 20){
+                Text("Animal Friends")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.bold)
+                        Text(animalName)
+                        Image(uiImage: inputImage!).resizable()
+                            .aspectRatio(contentMode: .fit)
+                        //Text(animalName)
+                        Button("Who am I?"){
+                            self.buttonPressed()
+                        }
+                        .padding(.all, 14.0)
+                        .foregroundColor(.white)
+                            .background(Color.green)
+                        .cornerRadius(10)
+            }
+            .font(.title)
+        }.sheet(isPresented: $showingImagePicker, onDismiss: processImage) {
+            ImagePicker(image: self.$inputImage)
+        }
+    }
+    
+    func buttonPressed() {
+        print("Button pressed")
+        self.showingImagePicker = true
+    }
+    
+    func processImage() {
+        self.showingImagePicker = false
+        self.animalName="Checking..."
+        guard let inputImage = inputImage else {return}
+        print("Processing image due to Button press")
+        let imageJPG=inputImage.jpegData(compressionQuality: 0.0034)!
+        let imageB64 = Data(imageJPG).base64EncodedData()
+        let uploadURL="https://askai.aiclub.world/957e82c0-623c-49b3-b867-ae7ed29b9496"
+        
+        AF.upload(imageB64, to: uploadURL).responseJSON { response in
+            
+            debugPrint(response)
+            switch response.result {
+               case .success(let responseJsonStr):
+                   print("\n\n Success value and JSON: \(responseJsonStr)")
+                   let myJson = JSON(responseJsonStr)
+                let predictedValue = myJson["predicted_label"].string
+                   print("Saw predicted value \(String(describing: predictedValue))")
+
+                   let predictionMessage = predictedValue!
+                   self.animalName=predictionMessage
+               case .failure(let error):
+                   print("\n\n Request failed with error: \(error)")
+               }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var image: UIImage?
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
     }
 }
