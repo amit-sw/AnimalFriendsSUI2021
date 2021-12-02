bd2242ced558d5cfa3ad15e41dad3099747b6edc
Amit Gupta <amit@gprof.com>
2021-12-02 11:55:27 -0800

    Initial Commit

---
+++ AnimalFriendsSUI2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimalFriendsSUI2021
+//
+//  Created by Amit Gupta on 12/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
